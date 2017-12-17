9c991a4c995654a42b8a7a78aaff39cc570f955c
dotSaviour <dotsaviourdev@gmail.com>
2017-12-17 22:02:32 +0300

    More Fixes

--- validate-registration.php
+++ validate-registration.php
@@ -8,8 +8,11 @@
     echo "<h1> Hello, $username! </h1>";
     echo "<div> <p>your email is $email </p>";
 
-    if($password == $passConf)
+    if($password == $passConf) {
         echo "<p> YOUR PASSWORD WILL NOT BE PRINTED . </p>";
+        if(sizeof($password) < 8)
+            echo "<p style='color:red;'> your password is less than 8 characters, it is not safe!</p>";
+    }
     else
         echo "Passwords do not match";
 
@@ -18,7 +21,7 @@
     if($about == 'I am...')
         echo "You haven't told us about your self.";
     else
-        echo "You wrote about your self : $about";
+        echo "You wrote about your self : " . htmlspecialchars($about); //To prevent Injection
 
     echo "</p></div>";
 ?>
